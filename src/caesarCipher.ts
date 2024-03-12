2223af3e7646d748791d90194d40ae0f563d5907
Bradley-Hill <brad.michael.hill.com>
2024-03-12 11:29:39 +0100

    TDD, added testing for the caesar shifting forward and backwards.

--- src/caesarCipher.ts
+++ src/caesarCipher.ts
@@ -1,6 +1,79 @@
 function caesarCipher(string: string, number: number): string {
-  // TODO! shifting mechanism/function
-  let shiftedString = string;
+  let alphabetDict = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+    I: 9,
+    J: 10,
+    K: 11,
+    L: 12,
+    M: 13,
+    N: 14,
+    O: 15,
+    P: 16,
+    Q: 17,
+    R: 18,
+    S: 19,
+    T: 20,
+    U: 21,
+    V: 22,
+    W: 23,
+    X: 24,
+    Y: 25,
+    Z: 26,
+  };
+
+  let numberDict = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J",
+    11: "K",
+    12: "L",
+    13: "M",
+    14: "N",
+    15: "O",
+    16: "P",
+    17: "Q",
+    18: "R",
+    19: "S",
+    20: "T",
+    21: "U",
+    22: "V",
+    23: "W",
+    24: "X",
+    25: "Y",
+    26: "Z",
+  };
+
+  let shiftedString = "";
+  let capitalString = string.toUpperCase();
+
+  for (let i = 0; i < capitalString.length; i++) {
+    let numberToShift =
+      alphabetDict[capitalString[i] as keyof typeof alphabetDict];
+    let newNumber = numberToShift + number;
+    if (newNumber > 26) {
+      newNumber = newNumber - 26;
+    } else if (newNumber < 0) {
+      newNumber = 26 + newNumber;
+    }
+    let cipherLetter = numberDict[newNumber as keyof typeof numberDict];
+    shiftedString += cipherLetter;
+  }
+  shiftedString = shiftedString.toLowerCase();
+
   return shiftedString;
 }
 module.exports = caesarCipher;
--- tests/caesarCipher.test.ts
+++ tests/caesarCipher.test.ts
@@ -1,9 +1,22 @@
 const caesarCipher = require("../src/caesarCipher");
 
 test("Expect a string to be returned", () => {
-  expect(caesarCipher("boob", 1)).toBe("boob");
+  expect(caesarCipher("boob", 0)).toBe("boob");
 });
 
 test("Expect the word to be shifted ahead by 1", () => {
   expect(caesarCipher("boob", 1)).toBe("cppc");
+});
+
+test("Expect the word to be shifted ahead by 11", () => {
+  expect(caesarCipher("rupert", 11)).toBe("cfapce");
+});
+
+test("Expect the word to be shifted backwards if negative number is provided", () => {
+  expect(caesarCipher("boob", -1)).toBe("anna");
+});
+("");
+
+test("Expect the word to be shifted backwards if large negative number is provided", () => {
+  expect(caesarCipher("rupert", -11)).toBe("gjetgi");
 });
